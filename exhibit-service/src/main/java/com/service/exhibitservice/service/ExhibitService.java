36dfafac17c8d52e27616e47b2cdd010276e3557
hyeokjin <gurwls2399@gmail.com>
2021-05-20 18:03:48 +0900

    feat : 주석 파일 변경

--- exhibit-service/src/main/java/com/service/exhibitservice/service/ExhibitService.java
+++ exhibit-service/src/main/java/com/service/exhibitservice/service/ExhibitService.java
@@ -30,7 +30,7 @@ public class ExhibitService {
 
     /**
      * 시간별 레시피 추천 데이터
-     * @return ResponseEntity<UserTimeRecommendRecipeResponseDto>
+     * @return UserTimeRecommendResponseDto
      */
     public UserTimeRecommendResponseDto getUserTimeRecommendRecipe() {
         //TODO 사용자 데이터 처리 부분 신규 작성 필요
@@ -63,7 +63,7 @@ public class ExhibitService {
 
     /**
      * 매주 지정된 레시피 10개 (이번주 도전 요리)
-     * @return ResponseEntity<List<WeeklyChallengeRecipeResponseDto>>
+     * @return List<WeeklyChallengeRecipeResponseDto>
      */
     public List<WeeklyChallengeRecipeResponseDto> getWeeklyChallengeRecipe() {
         //TODO 스크랩 부분 처리 필요
@@ -83,7 +83,7 @@ public class ExhibitService {
 
     /**
      * 주간 스크랩이 가장 많았던 레시피 10개 (이번주 베스트 레시피)
-     * @return ResponseEntity<List<WeeklyBestRecipeResponseDto>>
+     * @return List<WeeklyBestRecipeResponseDto>
      */
     public List<WeeklyBestRecipeResponseDto> getWeeklyBestRecipe() {
         //TODO 스크랩 부분 처리 필요
@@ -103,7 +103,7 @@ public class ExhibitService {
 
     /**
      * 인기 검색어 (1분동안 음식 검색에서 많이 검색된 음식 1~5위 노출)
-     * @return ResponseEntity<List<PopularSearchWordResponseDto>>
+     * @return List<PopularSearchWordResponseDto>
      */
     public List<PopularSearchWordResponseDto> getPopularSearchWord() {
         //TODO Redis 등을 이용해서 인기 검색어 만들지 확인 필요 (일단은 임시 데이터 작성)
@@ -138,7 +138,7 @@ public class ExhibitService {
 
     /**
      * 전체 레시피에서 스크랩 많은 순으로 10개 노출
-     * @return ResponseEntity<List<RecommendRecipeResponseDto>>
+     * @return List<RecommendRecipeResponseDto>
      */
     public List<RecommendRecipeResponseDto> getRecommendRecipe() {
         //TODO 스크랩 많은 순 적용 필요 (임시 데이터 작성)
@@ -159,8 +159,10 @@ public class ExhibitService {
         return recommendRecipeResponseDtoList;
     }
 
-    //TODO 검색 기록 바탕으로 레시피 노출
-
+    /**
+     * 홈 통합 데이터 조회
+     * @return HomeIntegratedResponseDto
+     */
     public HomeIntegratedResponseDto getHomeIntegrated() {
         return HomeIntegratedResponseDto.builder()
                 .userTimeRecommendRecipe(getUserTimeRecommendRecipe())
@@ -170,4 +172,6 @@ public class ExhibitService {
                 .recommendRecipeList(getRecommendRecipe())
                 .build();
     }
+
+    //TODO 검색 기록 바탕으로 레시피 노출
 }
